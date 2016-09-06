29cec5c362066bdc2e511a941c79c0de83f20c66
nathanrim99 <nathan.chan.rim@gmail.com>
2016-09-06 08:51:43 -0700

    Added Files

---
+++ APLesson_01/FirstProg.java
@@ -0,0 +1,16 @@
+public class FirstProg
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Hello World");
+		System.out.println("Hello Again");
+		System.out.println("  \"\"  \"\"    ");
+		System.out.println("^^^^");
+		System.out.println("lo     ol");
+		System.out.println("l   U    l");
+		System.out.println("l  ----  l");
+		System.out.println("-------");
+		
+		
+	}
+}
